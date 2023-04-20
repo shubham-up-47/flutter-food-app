bec41eb01344a2b86a0f2b1c1b49b3801ccc2c8c
Shubham Kaushal <shubham271306@gmail.com>
2023-04-20 17:13:05 +0530

    first commit

---
+++ foodapp/lib/auth/sign_in.dart
@@ -0,0 +1,119 @@
+import 'package:foodapp/main.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
+import 'package:foodapp/screens/home/home_screen.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class SignIn extends StatefulWidget{
+  @override
+  State<SignIn> createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+
+  _googleSignUp() async {
+    try {
+      final GoogleSignIn _googleSignIn = GoogleSignIn(
+        scopes: ['email'],
+      );
+      final FirebaseAuth _auth = FirebaseAuth.instance;
+
+      final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
+      final GoogleSignInAuthentication? googleAuth =
+          await googleUser?.authentication;
+
+      final AuthCredential credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth?.accessToken,
+        idToken: googleAuth?.idToken,
+      );
+
+      final User? user = (await _auth.signInWithCredential(credential)).user;
+      // print("signed in " + user.displayName);
+
+      return user;
+    } 
+    catch (e) {
+     // print(e.message);
+    }
+  }
+
+  @override 
+  Widget build(BuildContext context){
+    return Scaffold(
+
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            fit: BoxFit.cover,
+            image: AssetImage('assets/background.png')
+          ),
+        ),
+
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              height: 400,
+        //      color: Colors.red,
+              width: double.infinity,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text('Sign in to continue'),
+                  Text(
+                    'Vegi',
+                    style: TextStyle(
+                      fontSize: 50,
+                      color: Colors.white,
+                      shadows: [
+                        BoxShadow(
+                          blurRadius: 5,
+                          color: Colors.green.shade900,
+                          offset: Offset(3,3),
+                        )
+                      ]
+                    ) 
+                  ),
+                  Column(
+                    children: [
+                      SignInButton(
+                        Buttons.Apple,
+                        text: "Sign in with Apple",
+                        onPressed: () {},
+                      ),
+                      SignInButton(
+                        Buttons.Google,
+                        text: "Sign in with Google",
+                        onPressed: () {
+                        //  debugPrint("successfully logged in");
+                          _googleSignUp().then(
+                            (value) => Navigator.of(context).pushReplacement(
+                              MaterialPageRoute(
+                                builder: (context) => HomeScreen(),
+                              ),
+                            ),
+                          );
+                        },
+                      ),  
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Text('By signing in you are agreeing to our', style: TextStyle(color: Colors.grey[800]),),
+                      Text('Terms and Privacy Policy', style: TextStyle(color: Colors.grey[800]),),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+
+
+    );
+  }
+}
